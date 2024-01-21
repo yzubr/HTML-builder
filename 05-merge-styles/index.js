632e7d58b6f50df9865bf686d1ede61a07639e8e
yzubr <vadzim.vinnikau@gmail.com>
2024-01-21 22:04:18 +0300

    add:solution in task 05

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const bundle = fs.createWriteStream(path.join(__dirname, 'project-dist', "bundle.css"))
+
+fs.readdir(
+    path.join(__dirname, 'styles'),
+    (err, files) => {
+        if (err) {
+            return console.error(err);
+        };
+        files.forEach((file) => {
+            if (file.isFile() && path.extname(file) == ".css") {
+                const stream = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
+                stream.on('data', (chunk) => bundle.write(chunk));
+            }
+        })
+    })
