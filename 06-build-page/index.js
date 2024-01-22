150fe099defd84e73263670c70c8452a610e3856
yzubr <vadzim.vinnikau@gmail.com>
2024-01-22 22:07:52 +0300

    fix: bugs in task 05

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-function makeDir() {
-    fs.mkdir(path.join(__dirname, 'project-dist'),
+function makeDir(pathtonewfolder, foldername) {
+    fs.mkdir(path.join(pathtonewfolder, foldername),
         { recursive: true },
         (err) => {
             if (err) {
@@ -11,22 +11,55 @@ function makeDir() {
             console.log('Directory created successfully!');
         });
 }
+makeDir(__dirname, 'project-dist');
 
-function copyFiles() {
-    fs.readdir(
-        path.join(__dirname, 'assets'),
-        (err, files) => {
+// function copyFiles(pathtofolderforread,folderforread) {
+//     fs.readdir(
+//         path.join(pathtofolderforread, folderforread),
+//         (err, files) => {
+//             if (err) {
+//                 return console.error(err);
+//             };
+//             for (let file of files) {
+//                 fs.copyFile(path.join(pathtofolderforread, folderforread, file), path.join(__dirname, 'project-dist', file), (err) => {
+//                     if (err) {
+//                         return console.error(err);
+//                     }
+//                     console.log('Files copies successfully!');
+//                 });
+//             }
+//         })
+// }
+// copyFiles()
+
+function Assets() {
+    fs.readdir(path.join(__dirname, 'assets'),
+        { withFileTypes: true },
+        (err, folder) => {
             if (err) {
                 return console.error(err);
             };
-            for (let file of files) {
-                fs.copyFile(path.join(__dirname, 'assets', file), path.join(__dirname, 'project-dist', file), (err) => {
-                    if (err) {
-                        return console.error(err);
-                    }
-                    console.log('Files copies successfully!');
-                });
-            }
+            folder.forEach((el) => {
+                if (el.isDirectory) {
+                    makeDir(path.join(__dirname, 'project-dist', 'assets'), el.name);
+                    fs.readdir(
+                        path.join(__dirname, 'assets', el.name),
+                        (err, files) => {
+                            if (err) {
+                                return console.error(err);
+                            };
+                            for (let file of files) {
+                                console.log(path.parse(file).name)
+                                fs.copyFile(path.join(__dirname, 'assets', el.name, file), path.join(__dirname, 'project-dist', 'assets', el.name, file), (err) => {
+                                    if (err) {
+                                        return console.error(err);
+                                    }
+                                    console.log('Files copies successfully!');
+                                });
+                            }
+                        })
+                }
+            })
         })
 }
-copyFiles()
+Assets()
